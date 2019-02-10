f5948cb1acf65e11d879891d5e3d1574ac1fa853
Rahul D Shetty <35rahuldshetty@gmail.com>
2019-02-10 18:10:51 +0530

    bgged

--- parsegen.py
+++ parsegen.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 '''
 Rules
 1. Always terminate the production with ;
@@ -6,165 +9,186 @@ Rules
 4. Define rules for all variables used.
 5. # is used to denote empty string or Epsilon
 '''
-def first(symb,parser):
-	if symb is "|":return ["#"]
-	if symb in parser.terminals:
-		return [symb]
-	elif symb is "#":
-		return ["#"]
-	ans=[]
-	body=parser.prods[symb]
-	found=0
-	waitKey=0
-	for item in body:
-		if item=="|":
-			if waitKey==1:
-				ans.append("#")
-				waitKey=0
-			found=0
-			continue
-		if found==0:
-			if item is "#":
-				ans+=["#"]
-				break
-			elif item in parser.terminals:
-				waitKey=0
-				ans+=[item]
-			else:
-				subFirst = first(item,parser)
-				ans+=[x for x in subFirst if x != "#"]
-				if "#" in subFirst:
-					waitKey=1
-					continue
-			found=1
-	if waitKey==1:ans.append("#")
-	return list(set(ans))
 
-			
-def follow(symb,parser,isStart=False):
-	ans=[]
-	if isStart==True:
-		ans.append('$')
-	for prod in parser.prods:
-		body=parser.prods[prod]
-		if symb in body:
-			f=1
-			for item in body:
-				if f==0:
-                                        if item in parser.terminals:
-                                                f=1
-                                                ans+=[item]
-                                                continue
-                                        firstSet=first(item,parser)
-                                        ans+= [x for x in firstSet if x!='#']
-                                        if "#" in firstSet:
-                                                continue
-                                        f=1
-				if item is symb:
-					f=0
-					continue
-			if f==0:
-				ans+=follow(prod,parser)
-	return  list(set(ans))
+visited = {}
 
+
+def first(symb, parser):
+    if symb is '|':
+        return ['#']
+    if symb in parser.terminals:
+        return [symb]
+    elif symb is '#':
+        return ['#']
+    ans = []
+    body = parser.prods[symb]
+    found = 0
+    waitKey = 0
+    for item in body:
+        if item == '|':
+            if waitKey == 1:
+                ans.append('#')
+                waitKey = 0
+            found = 0
+            continue
+        if found == 0:
+            if item is '#':
+                ans += ['#']
+            elif item in parser.terminals:
+                waitKey = 0
+                ans += [item]
+            else:
+                subFirst = first(item, parser)
+                ans += [x for x in subFirst if x != '#']
+                if '#' in subFirst:
+                    waitKey = 1
+                    continue
+            found = 1
+    if waitKey == 1:
+        ans.append('#')
+    return list(set(ans))
+
+
+def follow(symb, parser, isStart=False):
+    if visited[symb] == 191:
+        return []
+    visited[symb] += 1
+
+    ans = []
+    if isStart == True:
+        ans.append('$')
+    for prod in parser.prods:
+        body = parser.prods[prod]
+        if symb in body:
+            f = 1
+            for item in body:
+                if f == 0:
+                    if item == '#':
+                        continue
+                    if item in parser.terminals:
+                        f = 1
+                        ans += [item]
+                        continue
+                    elif item == '|':
+                        if prod is parser.variables[0]:
+                            ans += follow(prod, parser, True)
+                        else:
+                            ans += follow(prod, parser)
+
+                        continue
+                    firstSet = parser.firstSet[item]
+                    ans += [x for x in firstSet if x != '#']
+                    if '#' in firstSet:
+                        continue
+                    f = 1
+                if item is symb:
+                    f = 0
+                    continue
+            if f == 0:
+                if prod is parser.variables[0]:
+                    ans += follow(prod, parser, True)
+                else:
+                    ans += follow(prod, parser)
+    visited[symb] = 0	
+    return list(set(ans))
 
 
 class Parser:
 
-	def __init__(self,code):
-		self.code=code
+    def __init__(self, code):
+        self.code = code
 
-	def createEmptyTable(self):
-		self.variables=[]
-		self.terminals=[]
-		# Get the variables
-		for var in self.prods.keys():
-			self.variables.append(var)
+    def createEmptyTable(self):
+        self.variables = []
+        self.terminals = []
 
-		self.variables=list(set(self.variables))
-		# get the teminals
-		for key,value in self.prods.items():
-			for item in value:
-				if item not in self.variables and item != "|" and item !='#':
-					self.terminals.append(item)
+        # Get the variables
 
-		self.terminals=list(set(self.terminals))		
-		'''
-		Table format
-		M   id      +       t3   t4 ...
-		E   id E1           
-		E1         + T E1
-		C 
-		D
-		'''
-		self.table=[  ]
-		# Each row is for one variable
-		for e in self.variables:
-			self.table.append([])
+        for var in self.prods.keys():
+            self.variables.append(var)
+            visited[var] = -1
 
-		# add columns for each row
-		for r in range(len(self.variables)):
-			for c in range(len(self.terminals)):
-				self.table[r].append([])
-		
+        self.variables = list(set(self.variables))
 
+        # get the teminals
 
+        for (key, value) in self.prods.items():
+            for item in value:
+                if item not in self.variables and item != '|' and item \
+                    != '#':
+                    self.terminals.append(item)
 
-	def processProductions(self):
-		#find each productions by using ; as splitter
-		prods=[x.strip() for x in self.code.split(';;')]
-		prods=[x for x in prods if len(x)!=0]
-		prods=[self.parseProduction(x) for x in prods]
-		prodsD={}
-		for item in prods:
-			head=item[0]
-			body=item[1]
-			prodsD[head]=body
-		self.prods=prodsD
-		
+        self.terminals = list(set(self.terminals))
+        self.table = []
 
+        # Each row is for one variable
 
-		self.createEmptyTable()
-		
-		
+        for e in self.variables:
+            self.table.append([])
 
-		firstSet={}
-		f=0
-		for prod in prodsD:
-			if f==0:
-				firstSet[prod]=first(prod,self)
-			else:
-				firstSet[prod]=first(prod,self)
+        # add columns for each row
 
-		print('First set:',firstSet)
+        for r in range(len(self.variables)):
+            for c in range(len(self.terminals)):
+                self.table[r].append([])
 
-		followSet={}
-		f=0
-		for prod in prodsD:
-			if f==0:
-				followSet[prod]=follow(prod,self,True)
-			else:
-				followSet[prod]=follow(prod,self)
+    def processProductions(self):
 
-		print("Follow Set:",followSet)
+        # find each productions by using ; as splitter
 
+        prods = [x.strip() for x in self.code.split(';;')]
+        prods = [x for x in prods if len(x) != 0]
+        prods = [self.parseProduction(x) for x in prods]
+        prodsD = {}
+        for item in prods:
+            head = item[0]
+            body = item[1]
+            prodsD[head] = body
+        self.prods = prodsD
 
-	def parseProduction(self,code):
-		# Production of the form A -> B | A ;
-		head,body=code.split('->')
-		head=head.strip()
-		body=[x.strip() for x in body.split()]
-		return (head,body)
+        self.createEmptyTable()
+
+        firstSet = {}
+        f = 0
+        for prod in prodsD:
+            if f == 0:
+                firstSet[prod] = first(prod, self)
+            else:
+                firstSet[prod] = first(prod, self)
+        self.firstSet = firstSet
+        print ('First set:', firstSet)
+
+        followSet = {}
+        f = 0
+        for prod in prodsD:
+            if f == 0 and prod == prods[0][0]:
+                followSet[prod] = follow(prod, self, True)
+                f = 1
+            else:
+                followSet[prod] = follow(prod, self)
+
+        print ('Follow Set:', followSet)
+
+    def parseProduction(self, code):
+
+        # Production of the form A -> B | A ;
+
+        (head, body) = code.split('->')
+        head = head.strip()
+        body = [x.strip() for x in body.split()]
+        return (head, body)
 
 
 # p="E -> T E1 ;; E1 ->  + T E1 ; | # ;; T -> id ;; "
-p = """
+
+p = \
+    """
 	S -> int main ( ) begin STMTS end ;;
 	STMTS -> STMT STMTS | # ;;
-	STMT -> datatype STMT1 ; | CONDITION | FUNCTION | id = E ;;
+	STMT -> datatype STMT1 ; | CONDITION | FUNCTION ; | id = E ;;
 	STMT1 -> id STMT2 ;;
-	STMT2 -> , id STMT2 | = E | # ;;
-	CONDITION -> if ( E ) begin STMT end ;;
+	STMT2 -> , id STMT2 | = E STMT2 | # ;;
+	CONDITION -> if ( E ) begin STMTS end ;;
 	FUNCTION -> printf ( MSG ) ;;
 	MSG -> string | id ;;
 	E -> T E1 ;;
@@ -176,6 +200,7 @@ p = """
 	H -> ( E ) | - H | id | digits ;;
 """
 
-
-par=Parser(p)
+par = Parser(p)
 par.processProductions()
+
+			
